b2282fe2a7cd9cc4a09dd11f6fff831b590d691f
fesus rocuts <fesusrocuts@gmail.com>
2020-07-16 22:08:36 -0500

    remove directory that writed bad

--- math/0x00-linear/11-the_western_exchange.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-""" Write a function def np_transpose(matrix):
-that transposes matrix:  """
-
-
-def np_transpose(matrix):
-    """ Write a function def np_transpose(matrix):
-    that transposes matrix:  """
-    return matrix.transpose()
--- math/0x00-linear/3-flip_me_over.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import numpy as np
-
-
-def matrix_transpose(matrix):
-    if type(matrix) is list:
-        return np.array(matrix).transpose()
-    return []
