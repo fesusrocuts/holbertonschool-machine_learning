fb75218b240e70fed9cc5f4c7ffe337cc40f1c87
fesus rocuts <fesusrocuts@gmail.com>
2020-07-19 23:53:20 -0500

    f5 v1

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -4,32 +4,21 @@ file 5-across_the_planes.py
 """
 
 
-def add_matrices2D(arr1, arr2):
+def add_matrices2D(mat1, mat2):
     """
     fn add_matrices2D that sum each key-value with other matrix
     Args:
-        arr1 (list of integers): The first parameter.
-        arr2 (list of integers): The second parameter.
+        mat1 (list of integers): The first parameter.
+        mat2 (list of integers): The second parameter.
     Returns:
-        arr3: The return new matrix for success, None otherwise.
+        mat3: The return new matrix for success, None otherwise.
     """
-    try:
-        arr3 = []
-        if len(arr1) != len(arr2) or type(arr1) != type(arr2):
-            raise Exception("Matrix are not have the same size")
-        for i in range(len(arr1)):
-            if len(arr1[i]) != len(arr2[i]):
-                raise Exception("Matrix are not have the same size")
-            if isinstance(arr1[i][0], (int, float)) is False:
-                raise Exception("arr1 are not have type allow")
-            if isinstance(arr1[i][1], (int, float)) is False:
-                raise Exception("arr1 are not have type allow")
-            if isinstance(arr2[i][0], (int, float)) is False:
-                raise Exception("arr2 are not have type allow")
-            if isinstance(arr2[i][1], (int, float)) is False:
-                raise Exception("arr2 are not have type allow")
-            arr3.append([arr1[i][0] + arr2[i][0],
-                        arr1[i][1] + arr2[i][1]])
-        return arr3
-    except Exception as e:
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
+
+    mat3 = []
+    for row1, row2 in zip(mat1, mat2):
+        mat3.append([])
+        for x, y in zip(row1, row2):
+            mat3[-1].append(x + y)
+    return mat3
