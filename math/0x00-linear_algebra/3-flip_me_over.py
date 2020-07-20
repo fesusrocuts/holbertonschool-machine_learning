75ba62f6ec99f66f974e41f8cd596b70d8696e2d
fesus rocuts <fesusrocuts@gmail.com>
2020-07-19 23:45:26 -0500

    f3 v2

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-import numpy as np
 
 
-def matrix_transpose(matrix):
-    if type(matrix) is list:
-        return np.array(matrix).transpose()
+def matrix_transpose(x):
+    """ fn matrix_transpose"""
+    if type(x) is list:
+        return [list(i) for i in zip(*x)]
     return []
