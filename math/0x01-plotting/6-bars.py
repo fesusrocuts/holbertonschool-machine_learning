9a5bd7ef55fe94fa4cc6582f4568ae068ff2313c
fesus rocuts <fesusrocuts@gmail.com>
2020-07-22 09:51:01 -0500

    f6 v4

--- math/0x01-plotting/6-bars.py
+++ math/0x01-plotting/6-bars.py
@@ -10,16 +10,16 @@ width = 0.5
 people = ['Farrah', 'Fed', 'Felicia']
 f = ['apples', 'bananas', 'oranges', 'peaches']
 c = ['red', 'yellow', '#ff8000', '#ffe5b4']
-pos = [None, fruit[0], fruit[0] + fruit[1], fruit[0] + fruit[1] + fruit[2]]
+p = [None, fruit[0], fruit[0] + fruit[1], fruit[0] + fruit[1] + fruit[2]]
 
 plt.yticks(np.arange(0, 90, 10))
 plt.ylim(0, 80)
 plt.ylabel('Quantity of Fruit')
 plt.suptitle('Number of Fruit per Person')
-v = plt.bar(people, fruit[0], width=width, color=c[0], label=f[0], bottom=pos[0])
-x = plt.bar(people, fruit[1], width=width, color=c[1], label=f[1], bottom=pos[1])
-y = plt.bar(people, fruit[2], width=width, color=c[2], label=f[2], bottom=pos[2])
-z = plt.bar(people, fruit[3], width=width, color=c[3], label=f[3], bottom=pos[3])
+v = plt.bar(people, fruit[0], width=width, color=c[0], label=f[0], bottom=p[0])
+x = plt.bar(people, fruit[1], width=width, color=c[1], label=f[1], bottom=p[1])
+y = plt.bar(people, fruit[2], width=width, color=c[2], label=f[2], bottom=p[2])
+z = plt.bar(people, fruit[3], width=width, color=c[3], label=f[3], bottom=p[3])
 # https://matplotlib.org/api/_as_gen/matplotlib.pyplot.legend.html
 plt.legend(handles=[v, x, y, z])
 plt.show()
