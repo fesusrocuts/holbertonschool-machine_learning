6c024ddfdb882e893215efa765d0320c125fee85
fesus rocuts <fesusrocuts@gmail.com>
2020-07-31 23:41:50 -0500

    f9 v5

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,7 +6,7 @@ that calculates sum_{i=1}^{n} i^2: """
 def summation_i_squared2(value, limLower, n):
     value += limLower**2
     limLower += 1
-    if limLower >= (n + 1):
+    if limLower > n:
         return value
     return summation_i_squared2(value, limLower, n)
 
@@ -15,6 +15,9 @@ def summation_i_squared(n):
     """ Write a function def summation_i_squared(n):
     that calculates sum_{i=1}^{n} i^2: """
     try:
-        return summation_i_squared2(0, 1, n)
+        if type(n) is int:
+            return summation_i_squared2(0, 1, n)
+        else:
+            return None
     except Exception as e:
         return None
