a18cc3fe358d52c1c55cc4910434a263f4499473
Fesus Rocuts <fesusrocuts@gmail.com>
2020-09-18 21:57:38 -0500

    f0 v4

--- supervised_learning/0x08-deep_cnns/0-inception_block.py
+++ supervised_learning/0x08-deep_cnns/0-inception_block.py
@@ -9,18 +9,18 @@ import tensorflow.keras as K
 def inception_block(A_prev, filters):
     """
     A_prev is the output from the previous layer
-    filters is a tuple or list containing F1, F3R, 
+    filters is a tuple or list containing F1, F3R,
         F3,F5R, F5, FPP, respectively:
             F1 is the number of filters in the 1x1 convolution
-            F3R is the number of filters in the 1x1 convolution before 
+            F3R is the number of filters in the 1x1 convolution before
                 the 3x3 convolution
             F3 is the number of filters in the 3x3 convolution
-            F5R is the number of filters in the 1x1 convolution before 
+            F5R is the number of filters in the 1x1 convolution before
                 the 5x5 convolution
             F5 is the number of filters in the 5x5 convolution
-            FPP is the number of filters in the 1x1 convolution after 
+            FPP is the number of filters in the 1x1 convolution after
             the max pooling
-    All convolutions inside the inception block should use a 
+    All convolutions inside the inception block should use a
     rectified linear activation (ReLU)
     Returns: the concatenated output of the inception block
     """
@@ -42,9 +42,12 @@ def inception_block(A_prev, filters):
                              padding='same', activation='relu',
                              kernel_initializer=init)(cnn5x5)
     pooling_ib = K.layers.MaxPool2D(pool_size=[3, 3], strides=1,
-                                 padding='same')(A_prev)
-    pooling_ib = K.layers.Conv2D(filters=FPP, kernel_size=1,
-                              padding='same', activation='relu',
-                              kernel_initializer=init)(pooling_ib)
+                                    padding='same')(A_prev)
+    pooling_ib = K.layers.Conv2D(
+            filters=FPP,
+            kernel_size=1,
+            padding='same',
+            activation='relu',
+            kernel_initializer=init)(pooling_ib)
     output = K.layers.concatenate([cnn1x1, cnn3x3, cnn5x5, pooling_ib])
     return output
