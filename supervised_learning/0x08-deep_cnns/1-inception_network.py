049df195e494e2486a3350e2aab5579bca67a975
Fesus Rocuts <fesusrocuts@gmail.com>
2020-09-18 23:44:59 -0500

    f1 v3

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -22,25 +22,25 @@ def inception_network():
     input_Data = K.Input(shape=(224, 224, 3))
 
     net = K.layers.Conv2D(filters=64, kernel_size=7, strides=2,
-                                padding='same', activation='relu',
-                                kernel_initializer=init)(input_Data)
+                          padding='same', activation='relu',
+                          kernel_initializer=init)(input_Data)
     net = K.layers.MaxPool2D(pool_size=3, strides=2,
-                                   padding='same')(net)
+                             padding='same')(net)
 
     net = K.layers.Conv2D(filters=64, kernel_size=1,
-                                padding='same', activation='relu',
-                                kernel_initializer=init)(net)
+                          padding='same', activation='relu',
+                          kernel_initializer=init)(net)
     net = K.layers.Conv2D(filters=192, kernel_size=3,
-                                padding='same', activation='relu',
-                                kernel_initializer=init)(net)
+                          padding='same', activation='relu',
+                          kernel_initializer=init)(net)
     net = K.layers.MaxPool2D(pool_size=3, strides=2,
-                                   padding='same')(net)
+                             padding='same')(net)
 
     net = inception_block(net, [64, 96, 128, 16, 32, 32])
     net = inception_block(net, [128, 128, 192, 32, 96, 64])
 
     net = K.layers.MaxPool2D(pool_size=3, strides=2,
-                                   padding='same')(net)
+                             padding='same')(net)
 
     net = inception_block(net, [192, 96, 208, 16, 48, 64])
     net = inception_block(net, [160, 112, 224, 24, 64, 64])
@@ -49,7 +49,7 @@ def inception_network():
     net = inception_block(net, [256, 160, 320, 32, 128, 128])
 
     net = K.layers.MaxPool2D(pool_size=3, strides=2,
-                                   padding='same')(net)
+                             padding='same')(net)
 
     net = inception_block(net, [256, 160, 320, 32, 128, 128])
     net = inception_block(net, [384, 192, 384, 48, 128, 128])
@@ -58,5 +58,5 @@ def inception_network():
 
     drop_Out = K.layers.Dropout(.4)(avg_Pool)
     out_Data = K.layers.Dense(1000, activation='softmax',
-                             kernel_initializer=init)(drop_Out)
+                              kernel_initializer=init)(drop_Out)
     return K.Model(input_Data, out_Data)
