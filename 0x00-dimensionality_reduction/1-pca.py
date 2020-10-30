6ce51adc23050425e4efd0275c9d1f36528ec755
Fesus Rocuts <fesusrocuts@gmail.com>
2020-10-29 22:50:30 -0500

    add t0 r1 v0

---
+++ 0x00-dimensionality_reduction/1-pca.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Write a function def pca(X, ndim):
+that performs PCA on a dataset:
+"""
+import numpy as np
+
+
+def pca(X, ndim):
+    """
+    Write a function def pca(X, ndim):
+    that performs PCA on a dataset:
+    """
+    return None
