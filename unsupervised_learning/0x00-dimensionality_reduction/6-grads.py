c4d12a73c2cd5f25eb7f6a07f9ced834e71bee58
fesus rocuts <fesusrocuts@gmail.com>
2020-11-01 23:03:36 -0500

    t6 v0

---
+++ unsupervised_learning/0x00-dimensionality_reduction/6-grads.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+fn def grads(Y, P): that calculates the gradients of Y
+"""
+import numpy as np
+Q_affinities = __import__('5-Q_affinities').Q_affinities
+
+
+def grads(Y, P):
+    """
+    fn def grads(Y, P): that calculates the gradients of Y
+    """
+    # variables
+    return None
