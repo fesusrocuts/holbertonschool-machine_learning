db3648d79dd80a705c6a0ac6de5a4bc43cc07a48
fesus rocuts <fesusrocuts@gmail.com>
2020-11-01 22:57:49 -0500

    t4 v0

---
+++ unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+function def P_affinities(X, tol=1e-5, perplexity=30.0):
+that calculates the symmetric P affinities of a data set:
+"""
+import numpy as np
+P_init = __import__('2-P_init').P_init
+HP = __import__('3-entropy').HP
+
+
+def P_affinities(X, tol=1e-5, perplexity=30.0):
+    """
+    function def P_affinities(X, tol=1e-5, perplexity=30.0):
+    that calculates the symmetric P affinities of a data set:
+    """
+    return None
