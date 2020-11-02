96e72259c01f0abed76439c930430c31a39f3270
fesus rocuts <fesusrocuts@gmail.com>
2020-11-01 22:39:19 -0500

    t1 v2

--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -11,4 +11,9 @@ def pca(X, ndim):
     Write a function def pca(X, ndim):
     that performs PCA on a dataset:
     """
-    return None
+    # list data for s and Vt variables
+    X_mean = X - np.mean(X, axis=0)
+    u, s, Vt = np.linalg.svd(X_mean)
+    W = Vt[:ndim].T
+    M = np.matmul(X_mean, W) 
+    return M
