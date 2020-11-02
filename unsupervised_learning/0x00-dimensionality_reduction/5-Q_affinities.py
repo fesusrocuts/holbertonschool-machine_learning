d67d93b44c62848185173ed780db9616d9e470ae
fesus rocuts <fesusrocuts@gmail.com>
2020-11-01 23:00:51 -0500

    t5 v0

---
+++ unsupervised_learning/0x00-dimensionality_reduction/5-Q_affinities.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+fn def Q_affinities(Y): that calculates the Q affinities:
+"""
+import numpy as np
+
+
+def Q_affinities(Y):
+        """
+        fn def Q_affinities(Y): that calculates the Q affinities:
+        """
+        return None
