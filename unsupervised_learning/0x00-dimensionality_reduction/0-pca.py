a5f13d1e4446434d6919f2ef0828fd23fa599ed4
fesus rocuts <fesusrocuts@gmail.com>
2020-11-01 22:25:45 -0500

    t0 v2

--- unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-
+"""
+fn pca that performs PCA on a dataset:
+"""
 
 import numpy as np
 
@@ -8,4 +10,19 @@ def pca(X, var=0.95):
     """
     placeholder
     """
-    return None
+
+    # list data for s and Vt variables
+    U, s, Vt = np.linalg.svd(X)
+
+    # cumulative for variance
+    s_sum = np.sum(s)
+    cumsum = np.cumsum(s)
+
+    # limit for variance
+    avg_var = cumsum / s_sum
+
+    # find idx non-zero, grouped with element
+    r = np.argwhere(avg_var >= var)[0, 0]
+
+    W = Vt[:r + 1].T
+    return (W)
