4f7e9358864a53ea0b10dce8acaee86305af20b5
Fesus Rocuts <fesusrocuts@gmail.com>
2020-11-13 20:30:28 -0500

    t12 v0

---
+++ unsupervised_learning/0x01-clustering/12-agglomerative.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" 12. Agglomerative"""
+
+
+import numpy as np
+import scipy.cluster.hierarchy
+import matplotlib.pyplot as plt
+
+
+def agglomerative(X, dist):
+    """ 12. Agglomerative"""
+    return None
