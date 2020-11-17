3bb901ce7e58ee1eaf009b69253adf63d4c0bb4c
fesus rocuts <fesusrocuts@gmail.com>
2020-11-16 19:58:11 -0500

    t6 v2

--- unsupervised_learning/0x02-hmm/6-baum_welch.py
+++ unsupervised_learning/0x02-hmm/6-baum_welch.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 """
 6. The Baum-Welch Algorithm
-function def baum_welch(Observations, Transition, Emission, Initial, iterations=1000): that performs the Baum-Welch algorithm for a hidden markov model:
+function def baum_welch(Observations, Transition, Emission, Initial,
+iterations=1000): that performs the Baum-Welch algorithm for
+a hidden markov model:
 
-    Observations is a numpy.ndarray of shape (T,) that contains the index of the observation
+    Observations is a numpy.ndarray of shape (T,)
+    that contains the index of the observation
         T is the number of observations
-    Transition is a numpy.ndarray of shape (M, M) that contains the initialized transition probabilities
+    Transition is a numpy.ndarray of shape (M, M)
+    that contains the initialized transition probabilities
         M is the number of hidden states
-    Emission is a numpy.ndarray of shape (M, N) that contains the initialized emission probabilities
+    Emission is a numpy.ndarray of shape (M, N)
+    that contains the initialized emission probabilities
         N is the number of output states
-    Initial is a numpy.ndarray of shape (M, 1) that contains the initialized starting probabilities
-    iterations is the number of times expectation-maximization should be performed
-    Returns: the converged Transition, Emission, or None, None on failure
+    Initial is a numpy.ndarray of shape (M, 1)
+    that contains the initialized starting probabilities
+    iterations is the number of times expectation-maximization
+    should be performed
+    Returns: the converged Transition,
+    Emission, or None, None on failure
 """
 
 
@@ -19,18 +27,5 @@ import numpy as np
 
 
 def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
-    """
-    6. The Baum-Welch Algorithm
-    function def baum_welch(Observations, Transition, Emission, Initial, iterations=1000): that performs the Baum-Welch algorithm for a hidden markov model:
-
-        Observations is a numpy.ndarray of shape (T,) that contains the index of the observation
-            T is the number of observations
-        Transition is a numpy.ndarray of shape (M, M) that contains the initialized transition probabilities
-            M is the number of hidden states
-        Emission is a numpy.ndarray of shape (M, N) that contains the initialized emission probabilities
-            N is the number of output states
-        Initial is a numpy.ndarray of shape (M, 1) that contains the initialized starting probabilities
-        iterations is the number of times expectation-maximization should be performed
-        Returns: the converged Transition, Emission, or None, None on failure
-    """
+    """ 6. The Baum-Welch Algorithm"""
     return None, None, None
