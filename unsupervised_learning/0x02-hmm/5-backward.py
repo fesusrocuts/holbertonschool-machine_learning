707a753a2b3c2fb52c7f6bd23986712ea3653b66
fesus rocuts <fesusrocuts@gmail.com>
2020-11-16 19:55:51 -0500

    t5 v2

--- unsupervised_learning/0x02-hmm/5-backward.py
+++ unsupervised_learning/0x02-hmm/5-backward.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
 """
 5. The Backward Algorithm
-function def backward(Observation, Emission, Transition, Initial): that performs the backward algorithm for a hidden markov model:
+function def backward(Observation, Emission, Transition, Initial):
+that performs the backward algorithm for a hidden markov model:
 
-    Observation is a numpy.ndarray of shape (T,) that contains the index of the observation
+    Observation is a numpy.ndarray of shape (T,)
+    that contains the index of the observation
         T is the number of observations
-    Emission is a numpy.ndarray of shape (N, M) containing the emission probability of a specific observation given a hidden state
-        Emission[i, j] is the probability of observing j given the hidden state i
+    Emission is a numpy.ndarray of shape (N, M) containing
+    the emission probability of a specific observation given
+    a hidden state
+        Emission[i, j] is the probability of observing j
+        given the hidden state i
         N is the number of hidden states
         M is the number of all possible observations
-    Transition is a 2D numpy.ndarray of shape (N, N) containing the transition probabilities
-        Transition[i, j] is the probability of transitioning from the hidden state i to j
-    Initial a numpy.ndarray of shape (N, 1) containing the probability of starting in a particular hidden state
+    Transition is a 2D numpy.ndarray of shape (N, N) containing
+    the transition probabilities
+        Transition[i, j] is the probability of transitioning
+        from the hidden state i to j
+    Initial a numpy.ndarray of shape (N, 1) containing the
+    probability of starting in a particular hidden state
     Returns: P, B, or None, None on failure
         Pis the likelihood of the observations given the model
-        B is a numpy.ndarray of shape (N, T) containing the backward path probabilities
-            B[i, j] is the probability of generating the future observations from hidden state i at time j
+        B is a numpy.ndarray of shape (N, T) containing the
+        backward path probabilities
+            B[i, j] is the probability of generating the future
+            observations from hidden state i at time j
 """
 
 
@@ -23,22 +33,5 @@ import numpy as np
 
 
 def backward(Observation, Emission, Transition, Initial):
-    """
-    5. The Backward Algorithm
-    function def backward(Observation, Emission, Transition, Initial): that performs the backward algorithm for a hidden markov model:
-
-        Observation is a numpy.ndarray of shape (T,) that contains the index of the observation
-            T is the number of observations
-        Emission is a numpy.ndarray of shape (N, M) containing the emission probability of a specific observation given a hidden state
-            Emission[i, j] is the probability of observing j given the hidden state i
-            N is the number of hidden states
-            M is the number of all possible observations
-        Transition is a 2D numpy.ndarray of shape (N, N) containing the transition probabilities
-            Transition[i, j] is the probability of transitioning from the hidden state i to j
-        Initial a numpy.ndarray of shape (N, 1) containing the probability of starting in a particular hidden state
-        Returns: P, B, or None, None on failure
-            Pis the likelihood of the observations given the model
-            B is a numpy.ndarray of shape (N, T) containing the backward path probabilities
-                B[i, j] is the probability of generating the future observations from hidden state i at time j
-    """
+    """ 5. The Backward Algorithm"""
     return None, None
