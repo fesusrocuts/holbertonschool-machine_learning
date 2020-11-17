79443b959c5679a1d43eb82470817ff22b06b8c9
fesus rocuts <fesusrocuts@gmail.com>
2020-11-16 19:43:03 -0500

    t0 v2

--- unsupervised_learning/0x02-hmm/0-markov_chain.py
+++ unsupervised_learning/0x02-hmm/0-markov_chain.py
@@ -27,9 +27,11 @@ def markov_chain(P, s, t=1):
         t is the number of iterations that the markov chain has been through
         Returns: a numpy.ndarray of shape (1, n) representing the probability of being in a specific state after t iterations, or None on failure
     """
-    if t < 1 or type(P).__module__ != np.__name__ or len(P.shape) != 2 or
+    if (t < 1 or 
+        type(P).__module__ != np.__name__ or 
+        len(P.shape) != 2 or
         P.shape[0] < 1 or P.shape[0] != P.shape[1] or
         type(s).__module__ != np.__name__ or len(s.shape) != 2 or
-        s.shape[1] != P.shape[0] or s.shape[0] != 1:
+        s.shape[1] != P.shape[0] or s.shape[0] != 1):
         return None
     return np.linalg.multi_dot([s] + [P for i in range(t)])
